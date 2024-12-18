f6d7b4512657c1b599675d1ddcbb290ad72758f4
AS-Danish <asdanish123@gmail.com>
2024-12-18 21:51:48 +0530

    did the changes in the password Model

--- backend/models/Password.model.js
+++ backend/models/Password.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const passwordSchema = new mongoose.Schema({
   userId: {
@@ -32,5 +33,23 @@ const passwordSchema = new mongoose.Schema({
     default: Date.now
   }
 });
+
+// Encrypt password before saving
+passwordSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Add method to decrypt password
+passwordSchema.methods.decryptPassword = async function() {
+  return this.password;
+};
 
 export const Password = mongoose.model('Password', passwordSchema); 
