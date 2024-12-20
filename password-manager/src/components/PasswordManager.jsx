fdc40af35529a3b2f35ec343b8383b2352152644
AS-Danish <asdanish123@gmail.com>
2024-12-20 15:43:26 +0530

    imported useMemo

--- password-manager/src/components/PasswordManager.jsx
+++ password-manager/src/components/PasswordManager.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { motion } from 'framer-motion'
 import { useUser, UserButton } from '@clerk/clerk-react'
 import PasswordGenerator from './PasswordGenerator'
